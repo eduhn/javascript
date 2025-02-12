40e502c6a51545f7eceef60d831eab93c9a98f31
Eduardo Henrique <eduhn1308@gmail.com>
2025-02-12 18:25:30 -0300

    Initial commit

---
+++ aulas/aula009/script.js
@@ -0,0 +1,4 @@
+var nm = window.prompt('Qual o seu nome?')
+var cor = window.prompt(`Olá, ${nm}! Qual cor você quer? (Red or Blue)`)
+if (cor == 'Red') document.body.style.backgroundColor = 'red';
+if (cor == 'Blue') document.body.style.backgroundColor = 'blue';body.style.backgoundColor = blue;
