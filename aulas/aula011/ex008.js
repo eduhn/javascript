dcc567ff803ad7c906d1d78f54b69b55a0a6e45b
Eduardo Henrique <eduhn1308@gmail.com>
2025-02-14 17:13:35 -0300

    Adicionar scripts para verificar velocidade do carro e nacionalidade

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,6 @@
+var v = 90
+console.log(`A velcidade do seu carro é ${v}Km/h.`)
+if(v>80){
+    console.log(`Você foi multado!`)
+}
+console.log(`Dirija sempre com responsabilidade.`)
---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,8 @@
+var p = 'Brasil'
+console.log(`Se você nasceu em ${p}..`)
+if(p!= 'Brasil'){
+console.log(`Você é Estrangeiro!`)
+}
+else{
+    console.log(`Você é Brasileiro!`)
+}
