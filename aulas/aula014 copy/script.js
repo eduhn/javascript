15e0a3c71cbd19288d3b8976b9700bafa21949e9
Eduardo Henrique <eduhn1308@gmail.com>
2025-02-19 20:04:56 -0300

    Alterar texto de entrada para ano de nascimento e ajustar estilo do botão; corrigir lógica de exibição de resultados

--- aulas/aula014 copy/script.js
+++ aulas/aula014 copy/script.js
@@ -1,15 +1,11 @@
     function resul() {
         var ida = document.querySelector('#ano');
         var sex = document.querySelector('#sex');
-        var res = document.querySelector('div#saida');
-        
-        if (sex == 'masc') {
-            sex = `Homem`;
-        }
-            else{
-                sex == `Mulher`;
-            }
+    var res = document.querySelector('div#saida');
+        var dat = new Date();
+        var anor = ano.getFullYear();
 
         res.innerHTML(`${ida}. ${sex}`);
+        //Voltar e resolver!!!
 
 }
