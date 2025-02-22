fa606fc024717574e18e9150ae58ebb5c612cc1a
Eduardo Henrique <eduhn1308@gmail.com>
2025-02-22 02:37:32 -0300

    adicionei os incremetos para a lógica básica

--- aulas/aula14ex/desafio16/scrpit copy.js
+++ aulas/aula14ex/desafio16/scrpit copy.js
@@ -1,8 +1,11 @@
 var a = 30;
 var b = 3;
 var c = 3;
+var d = 0
 
-for( b; b<=a; b = b + c){
-    console.log(`${b}`)
+for (b; b <= a; b = b + c) {
+    console.log(`${b}`);
+    var d = d + 1;
 }
-console.log(`Fim.`)
+
+console.log(`Fim! Foi necessário ${d} x ${c} para ${a}`);
