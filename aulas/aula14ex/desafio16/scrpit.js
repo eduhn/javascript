0c22c9f3c175fd7064e4dadb43bb86fefd83a7b4
Eduardo Henrique <eduhn1308@gmail.com>
2025-02-22 02:32:49 -0300

    Adicionar estilo ao HTML e aprimorar a lógica de contagem no JavaScript

--- aulas/aula14ex/desafio16/scrpit.js
+++ aulas/aula14ex/desafio16/scrpit.js
@@ -1,16 +1,24 @@
-/*function fc(){
-    var dvv = document.querySelector("div#nmr1");
-    var dvd = document.querySelector("div#nmr2");20
-    var dvd2 = document.querySelector("div#nmr2");
+function fc() {
+    var dvv = document.querySelector("#nmr1");
+    var dvd = document.querySelector("#nmr2");
+    var r = document.querySelector("#res");
 
-    
-    alert(`oi ${dvv} ${dvd} ${dvd2} ${d}`);
-};*/
+    var n1 = Number(dvv.value);
+    var n2 = Number(dvd.value);
+    var n3 = Number(dvd.value);
+    var c = 0;
 
-var d = prompt("Divisivel");
-var dd = prompt("Divisor");
-var c = prompt("Confirme o divisor")
-for(dd; dd<=d; dd = dd + c){
-    alert(`${dd}`);20
-};
-alert(`fim`);
+    r.innerHTML='';
+
+    for (n2; n2 <= n1; n2 = n2 + n3) {
+        var p = document.createElement("p")
+        p.textContent = `|✔️ ${n2}|`;
+        r.appendChild(p);
+        
+        var c = c + 1;
+    }
+    var t = document.createElement('h2');
+    t.textContent = `Fim!
+    Foi necessário ${c} x ${n3} para ${n1}`;
+    r.appendChild(t);
+}
